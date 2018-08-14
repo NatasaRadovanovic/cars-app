4b7b28e2a641f7e7d3bc0394af88a50700292dec
NatasaRadovanovic <natasar.radovanovic@gmail.com>
2018-08-14 19:00:29 +0200

    exercise1

---
+++ src/components/AppCars.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+  <h2>App Cars</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AppCars',
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Cars App</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+         <router-link to="/cars" class="nav-item nav-link">Cars</router-link>
+      </li>
+    </ul>
+  </div>
+</nav>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'NavBar',
+}
+</script>
+
+<style>
+
+</style>
