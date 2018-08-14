93daf81f62fea3e5e66ed6ee66aa3f9eadfa7c36
NatasaRadovanovic <natasar.radovanovic@gmail.com>
2018-08-14 19:41:50 +0200

    exercise2

--- src/components/AppCars.vue
+++ src/components/AppCars.vue
@@ -1,13 +1,42 @@
 <template>
   <div>
   <h2>App Cars</h2>
+   <div
+   class="list-group-item list-group-item"
+   v-for="(car, key) in cars" :key="key" 
+   exact-active-class = "active">
+   {{ car.brand }} <br>
+    {{ car.model }} <br>
+    {{ car.year }} <br>
+    {{ car.maxSpeed }} <br>
+    {{ car.isAutomatic }} <br>
+    {{ car.engine }} <br>
+    {{ car.numberOfDoors }} <br>
+   </div> 
   </div>
 </template>
 
 <script>
 
+import { cars } from '../services/Cars.js'
+
 export default {
   name: 'AppCars',
+
+  data(){
+      return{
+          cars:[]
+      }
+  },    
+    beforeRouteEnter (to, from, next) { 
+      cars.getAll()
+      .then(response =>{
+       next(vm => {
+         vm.cars = response.data
+       }) 
+      })
+      .catch(err => console.log(err))
+    },
 }
 </script>
 
